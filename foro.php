c70b92f2a8b7f6fb4e9b17667cb604460091ce34
jaime <jaimes10@ucm.es>
2024-03-09 22:45:53 +0100

    añadido al foro comentarios de ejemplo

--- foro.php
+++ foro.php
@@ -3,12 +3,21 @@
 session_start();
 
 require_once 'includes/config.php';
+require_once 'includes/vistas/helpers/autorizacion.php';
+//require_once 'includes/vistas/helpers/mensajes.php';
+require_once 'includes/vistas/helpers/foro.php';
+$tituloPagina = 'Foro'; 
 
-$tituloPagina = 'Foro';
+$contenidoPrincipal = '<h1>Bienvenido al Foro</h1>';
+$contenidoPrincipal .= mostrarListaForos();
 
-$contenidoPrincipal=<<<EOS
-	<h1>Discusión 1</h1>
-	<p>Comentario1</p>
-EOS;
+//$contenidoPrincipal .= listaMensajes();
+if (estaLogado()) {
+	//$formNuevoMensaje = mensajeForm('mensajes/nuevoMensaje.php', 'Nuevo mensaje: ', 'Crear');
+	$contenidoPrincipal .= <<<EOS
+		<h1>Nuevo Tablon</h1>
+		$formNuevoMensaje
+	EOS;
+}
 
 require 'includes/vistas/comun/layout.php';
