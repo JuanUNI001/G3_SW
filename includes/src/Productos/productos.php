88ccb1e0521dd95348469dc5f3ea507054b4011a
wsannnnw <swlodkow@ucm.es>
2024-03-10 19:06:19 +0100

    wr

--- includes/src/Productos/productos.php
+++ includes/src/Productos/productos.php
@@ -342,4 +342,6 @@ class Producto
         return $result;
     }
 
+    
+
 }
--- includes/src/caracteristicasProducto.php
+++ includes/src/caracteristicasProducto.php
@@ -1,74 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Detalles del Producto</title>
-    <link rel="stylesheet" type="text/css" href="/G3_SW/includes/views/estilo.css" />
-    <link rel="stylesheet" type="text/css" href="imagenes.css">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php 
+require_once 'includes/config.php';
+require_once 'includes/vistas/helpers/autorizacion.php';
+require_once 'includes/src/Productos/productos.php.php';
 
-</head>
-<body>
-    <?php 
-    require_once("BBDD.php");
+$tituloPagina = 'Producto';
 
-    // Verifica si se ha recibido el ID del producto
-    if(isset($_GET['id_producto'])) {
-        $id_producto = $_GET['id_producto'];
+$idPelicula = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-        // Consulta SQL para obtener la información del producto seleccionado
-        $sql = "SELECT id, nombre, descripción, precio, imagen, valoración, num_valoraciones FROM productos WHERE id = $id_producto";
-        $result = $conexion->query($sql);
+$pelicula = Producto::buscaPorId($id);
+if(!$pelicula){
+    Utils::paginaError(403, '','Error articulo');
+}
 
-        if ($result->num_rows > 0) {
-            $producto = $result->fetch_assoc(); // Obtiene los datos del producto
-            ?>
-            <div id="contenedor">
-                <?php require_once('/includes/src/vistas/comun/cabecera.php'); ?>
-                <?php require_once('/includes/src/vistas/comun/sidebarIzq.php'); ?>
+$contenidoPrincipal= visualizaProducto($pelicula);
 
-                <main>
-                    <article>
-                        <div class="producto_detalle">
-                            <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo $producto['nombre']; ?>" class="detalle_imagen">
-                            <h2><?php echo $producto['nombre']; ?></h2>
-                            <p><?php echo $producto['descripción']; ?></p>
-                            <p><strong>Precio:</strong> $<?php echo $producto['precio']; ?></p>
-                            
-                            <p><strong>Valoración:</strong> 
-                                    <?php
-                                    $valoracion = $producto['valoración'];
-                                    $valoracion_rounded = round($valoracion * 2) / 2; //redondeamos la valoracion pq no podemos hacer mitades
-                                    for ($i = 1; $i <= 5; $i++) {
-                                        if ($valoracion_rounded >= $i) {
-                                            echo '<span class="star">&#9733;</span>'; // Spawnea una estrella llena
-                                        } else {
-                                            echo '<span class="star">&#9734;</span>'; // Spawnea una estrella vacía
-                                        }
-                                    }
-                                    ?>
-                                
-                            / <?php echo $producto['num_valoraciones']; ?>valoraciones</p>
-                            <form action="agregar_al_carrito.php" method="post">
-                            <p>Cantidad:
-                                <input type="number" id="cantidad" name="cantidad" value="1" min="1" style="width: 50px;">
-                                <input type="hidden" name="id_producto" value="<?php echo $producto['id']; ?>">
-                                <input type="submit" value="Agregar al carrito">
-                            </p>
-                            </form>
-                        </div>
-                    </article>
-                </main>
-
-                <?php require_once('/includes/src/vistas/comun/sidebarDer.php'); ?>
-                <?php require_once('/includes/src/vistas/comun/pie.php'); ?>
-            </div>
-            <?php
-        } else {
-            echo "Producto no encontrado.";
-        }
-    } else {
-        echo "ID del producto no especificado.";
-    }
-    ?>
-</body>
-</html>
+require 'includes/vistas/comun/layout.php';
