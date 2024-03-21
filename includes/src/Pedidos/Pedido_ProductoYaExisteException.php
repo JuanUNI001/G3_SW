c26182e3f66088293071b720a3de1cbb28c5c84b
jaime <jaimes10@ucm.es>
2024-03-21 15:25:37 +0100

    Metodos pedidos y cambio execpciones

---
+++ includes/src/Pedidos/Pedido_ProductoYaExisteException.php
@@ -0,0 +1,11 @@
+<?php
+namespace es\ucm\fdi\aw;
+
+class PedidoYaExisteException extends \Exception
+{
+    function __construct(string $message = "" , int $code = 0 , Throwable $previous = null )
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
+
