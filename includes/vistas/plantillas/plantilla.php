ac17b76ca654acbacf6c738e9299db9f9c8ea2a1
anonymous <anonymous@anonymous>
2024-04-11 14:11:09 +0200

    pruebas

--- includes/vistas/plantillas/plantilla.php
+++ includes/vistas/plantillas/plantilla.php
@@ -12,8 +12,8 @@ $mensajes = mensajesPeticionAnterior();
 <?= $mensajes ?>
 <div id="contenedor">
 <?php
-$params['app']->doInclude('/vistas/comun/cabecera.php');
-// $params['app']->doInclude('/vistas/comun/sidebarIzq.php');
+$params['app']->doInclude('/vistas/comun/cabecera.php', $params);
+$params['app']->doInclude('/vistas/comun/sidebarIzq.php', $params);
 ?>
 	<main>
 		<article>
@@ -21,8 +21,8 @@ $params['app']->doInclude('/vistas/comun/cabecera.php');
 		</article>
 	</main>
 <?php
-// $params['app']->doInclude('/vistas/comun/sidebarDer.php');
-// $params['app']->doInclude('/vistas/comun/pie.php');
+$params['app']->doInclude('/vistas/comun/sidebarDer.php', $params);
+$params['app']->doInclude('/vistas/comun/pie.php', $params);
 ?>
 </div>
 </body>
