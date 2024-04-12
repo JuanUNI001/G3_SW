93317fc564cf85490d18827e3e464f14101e5450
anonymous <anonymous@anonymous>
2024-04-12 13:34:50 +0200

    cambiada distribucion de pagina web

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -2,6 +2,8 @@
 
 use es\ucm\fdi\aw\src\BD;
 use es\ucm\fdi\aw\src\Usuarios\FormularioLogout;
+use \es\ucm\fdi\aw\src\Usuarios\Usuario;
+
 
 function mostrarSaludo()
 {
@@ -24,10 +26,50 @@ function mostrarSaludo()
     return $html;
 }
 
+
+function mostrarInfoUsuario() {
+    $app = BD::getInstance();
+
+    if (!$app->usuarioLogueado())  {
+        $avatar = (RUTA_IMGS . 'images/avatarPorDefecto.png');
+        // Si el usuario no está logueado, muestra la foto por defecto
+        echo "<img src='{$avatar}' alt='Avatar por defecto' width='60' height='60'>";
+    } else {
+        $correo_usuario = $_SESSION['correo'];
+        $usuario = Usuario::buscaUsuario($correo_usuario);
+        $avatar = $usuario->getAvatar() ? (RUTA_IMGS . $usuario->getAvatar()) : (RUTA_IMGS . 'images/avatarPorDefecto.png');
+
+        echo "<div class='info-usuario'>";
+        echo "<div class='dropdown'>";
+        echo "<img src='{$avatar}' alt='Avatar de {$usuario->getNombre()}' width='60' height='60' class='avatar-dropdown'>";
+        echo "<ul class='dropdown-content'>";
+        echo "<li><a href='" . resuelve('/verPerfil.php') . "'>Ver mi cuenta</a></li>";
+        echo "<li><a href='" . resuelve('/verPedidosAnteriores.php') . "'>Pedidos anteriores</a></li>";
+        echo "<li><a href='" . resuelve('/includes/carrito_usuario.php') . "'>Carrito</a></li>"; // Enlace al carrito
+        echo "</ul></div></div>";
+    }
+}
+
 ?>
+
 <header>
-    <h1><?= $params['cabecera'] ?? 'Mesamaestra' ?></h1>
+    <h1><?= $params['cabecera'] ?? 'Mesa Maestra' ?></h1>
     <div class="saludo">
         <?= mostrarSaludo(); ?>
     </div>
+
+    <nav>
+        <ul>
+            <li><a href="<?= RUTA_APP?>/index.php"class="sideBarDerButton">Inicio</a></li>
+            <li><a href="<?= RUTA_APP?>/tienda.php" class="sideBarDerButton">Tienda</a></li>
+            <li><a href="<?= RUTA_APP?>/foros.php"class="sideBarDerButton">Foro</a></li>
+            <li><a href="<?= RUTA_APP?>/eventos.php"class="sideBarDerButton">Eventos</a></li>
+            <li><a href="<?= RUTA_APP?>/profesores.php"class="sideBarDerButton">Profesores</a></li>
+        </ul>
+    </nav>
+    <div class="info-usuario">
+        <?= mostrarInfoUsuario(); ?>
+    </div>
+
+
 </header>
