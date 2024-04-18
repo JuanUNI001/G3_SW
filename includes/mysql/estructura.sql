e7a10a17bf80cde848f9a178cd4bbf4164d3e44b
wsannnnw <swlodkow@ucm.es>
2024-04-18 09:41:53 +0200

    Inscritos BD

--- includes/mysql/estructura.sql
+++ includes/mysql/estructura.sql
@@ -250,3 +250,50 @@ CREATE TABLE `eventos` (
 ALTER TABLE `eventos`
   ADD PRIMARY KEY (`idEvento`);
 COMMIT;
+
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 15-04-2024 a las 17:22:52
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mesamaestra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscritos`
+--
+
+CREATE TABLE `inscritos` (
+  `idEvento` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `inscritos`
+--
+ALTER TABLE `inscritos`
+  ADD PRIMARY KEY (`idEvento`,`idUsuario`);
+COMMIT;
