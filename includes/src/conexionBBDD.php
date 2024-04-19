d3359aec82e0bbfc1c60abccb8f70b9cb26d998e
paulab020 <pbenegas@ucm.es>
2024-04-19 14:45:01 +0200

    elimnado

---
+++ includes/src/conexionBBDD.php
@@ -0,0 +1,14 @@
+<?php
+$server = 'localhost';
+$usuario = 'root';
+$password = ''; 
+$dbname = 'MesaMaestra'; 
+
+// Establecer la conexión
+$conexion = new mysqli($server, $usuario, $password, $dbname);
+
+// Verificar si hay errores de conexión
+if ($conexion->connect_error) {
+    die('<strong>No pudo conectarse:</strong> ' . $conexion->connect_error);
+}
+?>
