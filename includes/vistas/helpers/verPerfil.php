734a1bff7fae193a4383bc86b8c7fcfe0445f7cd
paulab020 <pbenegas@ucm.es>
2024-04-20 14:59:31 +0200

    edicion usuario funciona y css

--- includes/vistas/helpers/verPerfil.php
+++ includes/vistas/helpers/verPerfil.php
@@ -2,57 +2,54 @@
 
 use \es\ucm\fdi\aw\src\Usuarios\Usuario;
 use es\ucm\fdi\aw\src\BD;
+echo '<link rel="stylesheet" type="text/css" href="' . RUTA_CSS . '/estilos.css">';
+
 function mostrar_contenidoPerfil()
 {
-	$app = BD::getInstance();
+    $app = BD::getInstance();
     
     $contenido;
     if ($app->usuarioLogueado())  {
 
-		$correo_usuario = $_SESSION['correo'];
-		$usuario = Usuario::buscaUsuario($correo_usuario);
-		$nombre = $usuario->getNombre();
-		$rol = Usuario::rolUsuario($usuario);
-		$avatar = $usuario->getAvatar() ? (RUTA_IMGS . $usuario->getAvatar()) : (RUTA_IMGS . 'images/avatarPorDefecto.png');
-		$contenido =<<<EOS
-		<section>
-		<img src = $avatar width="140" height="140"> </img>
-		<h2> Informacion sobre mi perfil</h2>
-			<article>
-				<h3>Nombre de usuario:</h3>
-				<p>$nombre.</p>
-			</article>
+        $correo_usuario = $_SESSION['correo'];
+        $usuario = Usuario::buscaUsuario($correo_usuario);
+        $nombre = $usuario->getNombre();
+        $rol = Usuario::rolUsuario($usuario);
+        $avatar = $usuario->getAvatar() ? (RUTA_IMGS . $usuario->getAvatar()) : (RUTA_IMGS . 'images/avatarPorDefecto.png');
+        $direccionEditor = resuelve("EditorUsuarioView.php");
+        $imagenRuta = resuelve('/images/editar_producto.png');
 
-			<article>
-				<h3>Rol de usuario:</h3>
-				<p>$rol.</p>
-			</article>
+        $contenido =<<<EOS
+        <div class="perfil-container">
+            <section>
+                <img src="$avatar" width="140" height="140" alt="Avatar">
+                <h2>Información sobre mi perfil</h2>
+                <article>
+                    <h3>Nombre:</h3>
+                    <p>$nombre</p>
+                </article>
+                <article>
+                    <h3>Rol:</h3>
+                    <p>$rol</p>
+                </article>
+                <article>
+                    <h3>Correo electrónico:</h3>
+                    <p>$correo_usuario</p>
+                </article>
+                <div class="editar_Usuario">
+                    <a href="{$direccionEditor}?id={$usuario->getId()}">
+                        <img src="$imagenRuta" alt="Editor Producto" width="50" height="50">
+                    </a>   
+                </div>
+            </section>
+        </div>
+        EOS;
 
-			<article>
-				<h3>Correo electrónico:</h3>
-				<p>$correo_usuario.</p>
-			</article>
-			
-		</section>
-		EOS;
-
-		//if(isset($_SESSION["rol"]) === "admin"){
-			$direccionEditor = resuelve("EditorUsuarioView.php");
-			$imagenRuta=resuelve('/images/editar_producto.png');
-
-			$contenido .=<<<EOF
-			<div class="editar_Usuario">
-				<a href="{$direccionEditor}?id={$usuario->getId()}">
-					<img src="$imagenRuta" alt="Editor Producto" width="50" height="50">
-				</a>   
-			</div>
-			EOF; 
-		//}
-	} else {
+    } else {
         $contenido=<<<EOS
         <h2>Aviso:</h2>
         <p>Todavía no te has registrado.</p>
         EOS;
-	}
+    }
     return $contenido;
 }
--- verPerfil.php
+++ verPerfil.php
@@ -1,8 +1,8 @@
 <?php
 
 
-require_once __DIR__.'includes/config.php';
-require_once __DIR__.'includes/vistas/helpers/verPerfil.php';
+require_once 'includes/config.php';
+require_once 'includes/vistas/helpers/verPerfil.php';
 
 $tituloPagina = 'Ver_Perfil';
 
