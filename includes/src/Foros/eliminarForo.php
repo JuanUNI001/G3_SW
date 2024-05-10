aa2aaf9ee09e3242bfb1425a85240a2fae85e586
paulab020 <pbenegas@ucm.es>
2024-05-10 19:47:10 +0200

    foros

--- includes/src/Foros/eliminarForo.php
+++ includes/src/Foros/eliminarForo.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
     }
 }
-$url .= resuelve('foros.php');
+$url = resuelve('/foros.php');
 
 header("Location: $url");
 $app->putAtributoPeticion('mensajes', $mensajes);
